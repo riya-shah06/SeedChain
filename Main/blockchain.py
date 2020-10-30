a0f1012e7f610c979bde29bb920f25f6a064b42c
revathivijay <34963337+revathivijay@users.noreply.github.com>
2020-10-30 21:48:15 +0530

    Update blockchain.py

--- Main/blockchain.py
+++ Main/blockchain.py
@@ -2,10 +2,10 @@ from web3 import Web3
 import json
 
 ganache_url = "http://127.0.0.1:8545"
-w3 = Web3(Web3.HTTPProvider(ganache_url))
+web3 = Web3(Web3.HTTPProvider(ganache_url))
 
 #to check connection
-print(w3.isConnected())
+print(web3.isConnected())
 
 contract_address = web3.toChecksumAddress("")
 contract_abi = json.loads("")
