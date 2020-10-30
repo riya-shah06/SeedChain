b5ed8cc7a4803669f8d814c0387acd5b0f66f5b4
Rachana <rachanachaudhari16@gmail.com>
2020-10-31 05:02:29 +0530

    Before Starting Flask Backend

--- Main/routes.py
+++ Main/routes.py
@@ -1,7 +1,25 @@
 from flask import render_template, url_for, flash, redirect, request
 from Main import app
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/fetch_form", methods=["GET", "POST"])
+def fetch_form():
+    if request.method == "POST":
+        lot_number = request.form.get("lot_number")
+        if lot_number!= "":
+            return render_template('fetch_details.html', message=lot_number)
+    return render_template('fetch_form.html')
+
+# @app.route("/fetch_details")
+# def fetch_details(msg):
+#     return render_template('fetch_details.html', message=msg)
+
+@app.route("/create_profile")
+def create_profile():
+    return render_template('create_profile.html')
+
